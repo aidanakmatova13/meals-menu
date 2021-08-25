6e62e45dd407e8c52cb557475e0babeb290e8aa0
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-25 21:28:48 +0300

    initial

---
+++ src/MealDetails.js
@@ -0,0 +1,21 @@
+import {useParams} from 'react-router-dom';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const MealDetails = () =>{
+    const params = useParams()
+    const [meal, setMeal] = useState({})
+    useEffect(()=>{
+        axios(`https://gist.githubusercontent.com/juravlevdima/b239931140d1c3ae402a87b130f2caa6/raw/2aeee347830d20ec0720293d32905b0ae359526c/food.json`)
+            .then(({data}) => setMeal(data.find( (item) => item.id === params.id)))
+    }, [])
+    return (
+        <div>
+            <img src={meal.image} alt=""/>
+            <div className='meal-title'>Meal name: {meal.title}</div>
+            <div className='meal-desc'>A little description: {meal.description}</div>
+            <div>Price: {meal.price}</div>
+        </div>
+    )
+}
+export default MealDetails;
---
+++ src/Meals.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+const Meals = () =>{
+    const [users, setUsers] = useState([])
+    useEffect(() =>{
+        axios('https://gist.githubusercontent.com/juravlevdima/b239931140d1c3ae402a87b130f2caa6/raw/2aeee347830d20ec0720293d32905b0ae359526c/food.json')
+            .then(({data}) => setUsers(data))
+    }, [])
+    return (
+        <div className='row'>
+            {
+                users.map(item =>
+                    <>
+                        <div className='col-3'>
+                            <div className='box'>
+                                <Link  to={`/meal/${item.id}`}>
+                                    <img key={item.image} width='300' src={item.image.slice(0,60)} alt=""/>
+                                    <h3 key={item.id}>{item.title}</h3>
+                                </Link>
+                            </div>
+                        </div>
+                    </>
+                )
+            }
+        </div>
+    )
+}
+export default Meals;
