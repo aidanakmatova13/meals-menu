c2b5a6d8ffa7e508ebe4f30c35625193615ba5e0
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-26 04:45:25 +0300

    add comment

--- src/MealDetails.js
+++ src/MealDetails.js
@@ -7,7 +7,7 @@ const MealDetails = () =>{
     const [meal, setMeal] = useState({})
     useEffect(()=>{
         axios(`https://gist.githubusercontent.com/juravlevdima/b239931140d1c3ae402a87b130f2caa6/raw/2aeee347830d20ec0720293d32905b0ae359526c/food.json`)
-            .then(({data}) => setMeal(data.find( (item) => item.id === params.id)))
+            .then(({data}) => setMeal(data.find(item => item.id === params.id)))
     }, [])
     return (
         <div className='meal-content'>
@@ -15,6 +15,7 @@ const MealDetails = () =>{
             <div className='meal-title'>Meal name: {meal.title}</div>
             <div className='meal-desc'>A little description: {meal.description}</div>
             <div>Price: {meal.price}</div>
+            <div>Availability: {meal.inStock ? "yes" : "no"}</div>
         </div>
     )
 }
